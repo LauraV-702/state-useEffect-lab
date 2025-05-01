86b46a88ca6f3112e8940ee2cd911dc52cdbfe96
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-05-01 13:59:16 -0700

    Added comments for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import "./App.css";
 import { useState, useEffect } from "react";
 
 function App() {
+  //Hold the list of Pokemon from the API
   const [pokemons, setPokemons] = useState([]);
+  // Offset, starts at 0, then increase by 20
   const [offset, setOffset] = useState(0);
+  //details of the selected Pokemon
   const [selectedPokemon, setSelectedPokemon] = useState(null);
 
+  //fetch the set of Pokemon when offset changes
   useEffect(() => {
     fetchPokemons();
   }, [offset]);
 
+  //Fetch 20 Pokemon from the API based on the current offset
   const fetchPokemons = async () => {
     const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=20`
@@ -18,18 +23,20 @@ function App() {
     setPokemons(data.results);
   };
 
+  //NEXT
   const handleNext = () => {
     setOffset(offset + 20);
     setSelectedPokemon(null);
   };
-
-  const handleBack = () => {
+  //PREV
+  const handlePrev = () => {
     if (offset >= 20) {
       setOffset(offset - 20);
       setSelectedPokemon(null);
     }
   };
 
+  //when clicked, the pokemon details will display
   const handlePokemonClick = async (name) => {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
     const data = await res.json();
@@ -39,7 +46,7 @@ function App() {
   return (
     <div className="App">
       <h1>Pokémon List</h1>
-
+      {/* Display the grid of pokemon names */}
       <div className="card-container">
         {pokemons.map((pokemon) => (
           <div
@@ -52,13 +59,15 @@ function App() {
         ))}
       </div>
 
+        {/* pages changes buttons */}
       <div className="buttons">
-        <button onClick={handleBack} disabled={offset === 0}>
-          Back
+        <button onClick={handlePrev} disabled={offset === 0}>
+          Prev
         </button>
         <button onClick={handleNext}>Next</button>
       </div>
 
+        {/* display the pokemon details */}
       {selectedPokemon && (
         <div className="pokemon-details">
           <h2>{selectedPokemon.name}</h2>
